2b4c35f6fb1281a76875917213f86924b3ee625d
Wangshengyang2004 <wangshengyang2004@163.com>
2023-10-19 10:14:25 +0800

    Changes to be committed: 	new file:   data-BTC-USDT-from-2022-09-01-to-2023-10-01.csv 	new file:   data-BTC-USDT-from-2022-09-01-to-2023-10-05.csv 	new file:   data-BTC-USDT-from-2023-09-01-to-2023-10-01.csv 	new file:   data1.csv 	modified:   get_history_price.py 	new file:   webui.py

--- get_history_price.py
+++ get_history_price.py
@@ -17,29 +17,42 @@ def fetch_data_between_dates(instId, start_date, end_date, bar, limit=100):
     end_timestamp = date_to_timestamp(end_date)
     
     bar_to_milliseconds = {
-        '1D': 86400000,
+        '1Min': 60000,
+        '5Min': 300000,
+        '15Min': 900000,
+        '30Min': 1800000,
         '1H': 3600000,
-        '1M': 86400000 * 30  # Approximation, assuming 30 days in a month
-        # ... add other intervals as needed
+        '4H': 14400000,
+        '1D': 86400000,
+        '1W': 604800000
     }
     
     interval_milliseconds = bar_to_milliseconds[bar] * limit
     before_timestamp = start_timestamp
     after_timestamp = before_timestamp + interval_milliseconds
     
-    while after_timestamp < end_timestamp:
+    fix = 0
+    while after_timestamp <= end_timestamp:
         print("Fetching data between {} and {}".format(pd.Timestamp(after_timestamp, unit='ms'), pd.Timestamp(before_timestamp, unit='ms')))
         result = market.get_history_index_candles(instId=instId, bar=bar, limit=str(limit), before=before_timestamp, after=after_timestamp)
-        print(result)
+        # print(result)
         if result and 'data' in result:
             all_data.extend(result['data'])
-            print(all_data)
+            # print(all_data)
         else:
             print("Failed to fetch data for the current interval.")
         # print(result['data'])
         after_timestamp += interval_milliseconds
         before_timestamp += interval_milliseconds
-        time.sleep(0.5)  # To avoid hitting API rate limits
+
+        # If after_timestamp exceeds end_timestamp, adjust it
+        if after_timestamp >= end_timestamp:
+            after_timestamp = end_timestamp -1
+            fix = fix + 1
+        
+        if fix > 1:
+            break
+        time.sleep(0.1)  # To avoid hitting API rate limits
     
     return all_data
 
@@ -52,6 +65,12 @@ def convert_to_dataframe(result):
     return df
 
 if __name__ == '__main__':
-    data = fetch_data_between_dates(instId='BTC-USDT', start_date='2023-01-01', end_date='2023-10-08', bar="1H")
+    instId='BTC-USDT'
+    start_date='2022-09-01'
+    end_date='2023-10-05'
+    bar='1D'
+    data = fetch_data_between_dates(instId, start_date, end_date, bar)
     df = convert_to_dataframe(data)
-    df.to_csv("data.csv", index=False)
+    # Sort the data by 'Timestamp'
+    df_sorted = df.sort_values(by='Timestamp')
+    df_sorted.to_csv(f"data-{instId}-from-{start_date}-to-{end_date}.csv", index=False)
